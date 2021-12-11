23d5d7f7959de1d1235ef97cc987b852647db638
strumf04 <matejatomic04@gmail.com>
2021-12-11 19:51:25 +0100

    listaevent

---
+++ hzsfront/src/views/EventList.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <event-short
+      v-for="event in eventList"
+      v-bind:key="event._id"
+      :id="event._id"
+      :city="event.city"
+      :eventDay="event.eventDay"
+    ></event-short>
+  </section>
+</template>
+
+<script>
+import Event from './Event.vue'
+export default {
+  data() {
+    return {
+      eventList: [],
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
