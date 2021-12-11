1ac9b5f413e4cbf661f428eaa9b523b8226b6040
strumf04 <matejatomic04@gmail.com>
2021-12-12 00:45:20 +0100

    tekstZabice

---
+++ hzsfront/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+<div class='home'>
+<h5>Tim Žabice čine učenici Kraljevačke Gimnazije. Čiji je glavni cilj očuvanje prirode i gradova Srbije. Smatramo da je najbolji način za podsticanje ljudi na očuvanje i ne zagadjivanje prirode putem dogadjaja od kojih će imati benefit, pre svega priroda, ali i sami učesni.</h5>
+</div>
+</template>
