539e0237ab9160b713b0b7a12c4b18cfeb443c3c
strumf04 <matejatomic04@gmail.com>
2021-12-11 18:56:08 +0100

    sveTomic

--- hzsfront/src/router/index.js
+++ hzsfront/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
+import CreateEvent from '../views/CreateEvent.vue'
 const routes = [
   {
     path: '/',
@@ -17,6 +18,11 @@ const routes = [
     path: '/SignUp',
     name:'SignUp',
     component: SignUp
+  },
+  {
+    path: '/CreateEvent',
+    name:'CreateEvent',
+    component: CreateEvent
   },
   {
     path: '/about',
