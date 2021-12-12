153334a3ae6474b42d0c6ec40a460364b967eef9
DujaLegendaa <dorizmeo2@gmail.com>
2021-12-12 10:48:54 +0100

    ajde

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,24 +6,22 @@
     </h1>
     <p id="email">Email: {{ email }}</p>
     <p id="orgName" v-if="isOrganizer">Ime organizacije: {{ orgName }}</p>
-
-    <p>Poeni: {{ points }}</p>
     <p>Grad: {{ city }}</p>
 
     <div class="sec-2">
       <span class="first">
         <p>SBB poeni:</p>
-        <p>{{points[0]}}</p>
+        <p>{{ points[0] }}</p>
       </span>
 
       <span class="second">
         <p>Gigatron poeni:</p>
-        <p>{{points[1]}}</p>
+        <p>{{ points[1] }}</p>
       </span>
 
       <span class="third">
         <p>Tehnomedia poeni:</p>
-        <p>{{points[2]}}</p>
+        <p>{{ points[2] }}</p>
       </span>
     </div>
   </section>
@@ -69,21 +67,21 @@ export default {
 </script>
 
 <style scoped>
-
-.first, .second, .third{
+.first,
+.second,
+.third {
   display: grid;
   padding: 2% 5%;
   border: 0.2ch solid rgb(155, 155, 155);
   border-radius: 8px;
 }
 
-.sec-2{
+.sec-2 {
   display: grid;
   margin: 3ch 0;
   grid-template-columns: repeat(3, 1fr);
   grid-column-gap: 10px;
 }
-
 
 h4 {
   margin: 1% 0 3%;
