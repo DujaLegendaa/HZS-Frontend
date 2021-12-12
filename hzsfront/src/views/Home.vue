7ccd1c80ff09772a10219039eb1920bea9b7ea71
strumf04 <matejatomic04@gmail.com>
2021-12-12 08:25:20 +0100

    tekst

--- hzsfront/src/views/Home.vue
+++ hzsfront/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
     <div class='home'>
         <img id="background" src="../assets/frog3.png"/>
+        <h5>Idejno rešenje kroz ceo proces rada je ostalo isto, jer smo identifikovali veliki problem u Srbiji, koji se tiče zagadjenja. Trenutno aktuelna tema su kompanije koje ne vode računa o životnoj sredini i iz tog razloga gube poverenje naroda. Zelena budućnost bi rešila ovaj problem!
+To je aplikacija koja omogućava kompanijama, firmama ili manjim preduzećima da organizuju akcije čišćenja životne sredine u određenom gradu. Učesnici u čišćenju su fizička lica koja se prijavljuju na događaj putem aplikacije. Po završenom događaju, postojao bi QR kod koji bi učesnici skenirali i na taj način dobili poene od organizatora, koje bi kasnije mogli da iskoriste za neke popuste za usluge ili proizvode organizatora.</h5>
     </div>
 </template>
 
