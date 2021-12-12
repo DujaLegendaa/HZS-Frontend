05f113eeeb26d23772349749ed66f90306c97e2b
DujaLegendaa <dorizmeo2@gmail.com>
2021-12-12 03:01:59 +0100

    spajanje

--- hzsfront/src/components/CodeCard.vue
+++ hzsfront/src/components/CodeCard.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="base">
-    <p></p>
+    <p>For: {{ forUser }}</p>
+    <p>Points: {{ points }}</p>
     <img />
   </div>
 </template>
 
 <script>
-export default {};
+// if activated add custom css
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    points: {
+      type: Number,
+      required: true,
+    },
+    forUser: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
--- hzsfront/src/views/Codes.vue
+++ hzsfront/src/views/Codes.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="base">
-    <code-card v-for="code in codeList"></code-card>
+    <code-card
+      v-for="code in codeList"
+      :key="code._id"
+      :id="code._id"
+      :points="code.points"
+    ></code-card>
   </div>
 </template>
 
 <script>
+import CodeCard from "./../components/CodeCard.vue";
+const axios = require("axios");
 export default {
+  components: { CodeCard },
   data() {
     return {
       codeList: [],
@@ -15,12 +23,15 @@ export default {
     const that = this;
     var config = {
       method: "get",
-      url: process.env.VUE_APP_URL + "codes/",
-      headers: {},
+      url:
+        process.env.VUE_APP_URL + "events/" + this.$route.params.id + "/codes",
+      headers: {
+        Authorization: "Bearer " + this.$store.getters.token,
+      },
     };
     axios(config)
       .then(function (response) {
-        that.codeList = response.data.data.docs;
+        that.codeList = response.data.data.doc;
       })
       .catch(function (error) {
         console.log(error);
